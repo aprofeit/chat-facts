3ff134db2312cdaef8e3050e2d6786e784bd3733
Alexander Profeit <alex@profeit.com>
2022-12-03 03:21:05 -0500

    importing messages properly

---
+++ test/lib/calculator_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class CalculatorTest < ActiveSupport::TestCase
+  attr_reader :messages
+
+  setup do
+    @messages = JSON.load_file(Rails.root.join('test', 'fixtures', 'files', 'messages.json'))
+  end
+
+  test 'Alexander has the expected amount of messages' do
+    actual_message_count = @messages['messages'].filter { |m| m['sender_name'] == 'Alexander Profeit' }.size
+
+    assert_equal 59, actual_message_count
+  end
+
+end
