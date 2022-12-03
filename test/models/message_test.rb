cc186e8125a9cbc6b433f737c7908bf17ddd6846
Alexander Profeit <alex@profeit.com>
2022-12-03 03:39:03 -0500

    add reactions test

--- test/models/message_test.rb
+++ test/models/message_test.rb
@@ -1,4 +1,13 @@
 require "test_helper"
 
 class MessageTest < ActiveSupport::TestCase
+  test 'there should be 72 reacdted messages' do
+    assert_equal 72, messages.filter { |m| m['reactions'] }.size
+  end
+
+  private
+
+  def messages
+    @message ||= JSON.load_file(Rails.root.join('test', 'fixtures', 'files', 'messages.json'))['messages']
+  end
 end
