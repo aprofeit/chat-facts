6e6ed0eae422f3ea2a4c5cafbf4ea368ceef83fb
Alexander Profeit <alex@profeit.com>
2022-12-03 02:34:07 -0500

    records exist in controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @records = [
+      {
+        label: 'Sent messages',
+        name: 'Alexander Profeit',
+        value: 45
+      },
+      {
+        label: 'Received Reactions',
+        name: 'Yannis Fotiadis',
+        value: 17
+      }
+    ]
   end
 end
